9a9fe15ba8b5d118d789accd6787c1274501f13d
Anaïs Zafiriou-Lejeune <anais.ljn@outlook.fr>
2024-01-30 15:24:28 +0100

    Ajout du texte manquant dans la page about

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -8,10 +8,10 @@ function About() {
         <>
             <Background fileName="paysage-montagne.png" />
             <div className="about-content">
-                <Dropdown title="Fiabilité"></Dropdown>
-                <Dropdown title="Respect"></Dropdown>
-                <Dropdown title="Service"></Dropdown>
-                <Dropdown title="Sécurité"></Dropdown>
+                <Dropdown title="Fiabilité" text="Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes."></Dropdown>
+                <Dropdown title="Respect" text="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."></Dropdown>
+                <Dropdown title="Service" text="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."></Dropdown>
+                <Dropdown title="Sécurité" text="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."></Dropdown>
             </div>
         </>
     );
